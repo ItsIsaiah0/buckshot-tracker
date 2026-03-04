808eb5f2586632832776f8532a58671c30e860da
ItsIsaiah0 <isaiahnapoli52@gmail.com>
2026-03-04 12:56:56 -0500

    modified: index.html modified: script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const liveInput = document.getElementById('live-input');
+const blankInput = document.getElementById('blank-input');
+const livePercentText = document.getElementById('live-percent');
+const blankPercentText = document.getElementById('blank-percent');
 
+function updateOdds() {
+
+    let lives = Number(liveInput.value) || 0;
+    let blanks = Number(blankInput.value) || 0;
+
+    let total = lives + blanks;
+
+    if (total === 0) {
+        livePercentText.innerText = "0%";
+        blankPercentText.innerText = "0%";
+        return;
+    }
+
+    let liveChance = Math.round((lives / total) * 100);
+    let blankChance = Math.round((blanks / total) * 100);
+
+    livePercentText.innerText = liveChance + "%";
+    blankPercentText.innerText = blankChance + "%";
+}
+
+liveInput.addEventListener('input', updateOdds);
+blankInput.addEventListener('input', updateOdds);
